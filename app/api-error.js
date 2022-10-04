7ee1fab7892d3a639ae805b8707b4611232eb691
lecammo <113898641+lecammo@users.noreply.github.com>
2022-10-04 13:44:39 +0700

    Cai dat xu ly loi

---
+++ app/api-error.js
@@ -0,0 +1,8 @@
+class ApiErro extends Error {
+    constructor(statusCode, message){
+        super();
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
+module.exports = ApiErro;
